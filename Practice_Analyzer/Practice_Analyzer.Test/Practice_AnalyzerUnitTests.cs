47c7b49bc24bb57fe54aeb17b314f13d12daa5ec
staro <staro@MIRODEMPC>
2025-06-17 20:40:35 +0300

    Добавьте файлы проекта.

---
+++ Practice_Analyzer/Practice_Analyzer.Test/Practice_AnalyzerUnitTests.cs
@@ -0,0 +1,150 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = Practice_Analyzer.Test.CSharpCodeFixVerifier<
+    Practice_Analyzer.Practice_AnalyzerAnalyzer,
+    Practice_Analyzer.Practice_AnalyzerCodeFixProvider>;
+
+namespace Practice_Analyzer.Test
+{
+    [TestClass]
+    public class NamingConventionTests
+    {
+        // Тест для проверки имен классов
+        [TestMethod]
+        public async Task ClassNameStartsWithLower_ReportsDiagnostic()
+        {
+            var test = @"
+public class {|#0:myClass|} 
+{
+}";
+
+            var expected = VerifyCS.Diagnostic("NamingViolation")
+                .WithLocation(0)
+                .WithMessage("Class 'myClass' should start with uppercase letter");
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        // Тест для приватных свойств
+        [TestMethod]
+        public async Task PrivatePropertyStartsWithLower_ReportsDiagnostic()
+        {
+            var test = @"
+public class Test
+{
+    private string {|#0:myProperty|} { get; set; }
+}";
+
+            var expected = VerifyCS.Diagnostic("NamingViolation")
+                .WithLocation(0)
+                .WithMessage("Private property 'myProperty' should start with uppercase letter");
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        // Тест для событий
+        [TestMethod]
+        public async Task EventStartsWithLower_ReportsDiagnostic()
+        {
+            var test = @"
+using System;
+public class Test
+{
+    public event Action {|#0:myEvent|};
+}";
+
+            var expected = VerifyCS.Diagnostic("NamingViolation")
+                .WithLocation(0)
+                .WithMessage("Event 'myEvent' should start with uppercase letter");
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        // Тест для констант
+        [TestMethod]
+        public async Task ConstStartsWithLower_ReportsDiagnostic()
+        {
+            var test = @"
+public class Test
+{
+    public const int {|#0:maxValue|} = 100;
+}";
+
+            var expected = VerifyCS.Diagnostic("NamingViolation")
+                .WithLocation(0)
+                .WithMessage("Constant 'maxValue' should start with uppercase letter");
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        // Тест для локальных переменных
+        [TestMethod]
+        public async Task LocalVarStartsWithUpper_ReportsDiagnostic()
+        {
+            var test = @"
+public class Test
+{
+    public void Method()
+    {
+        int {|#0:MyVar|} = 0;
+    }
+}";
+
+            var expected = VerifyCS.Diagnostic("NamingViolation")
+                .WithLocation(0)
+                .WithMessage("Local variable 'MyVar' should start with lowercase letter");
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        // Тест, когда все имена корректны - не должно быть диагностик
+        [TestMethod]
+        public async Task AllNamesCorrect_NoDiagnostics()
+        {
+            var test = @"
+using System;
+public class CorrectClass
+{
+    private string CorrectProperty { get; set; }
+    public event Action CorrectEvent;
+    public const int CorrectConstant = 100;
+    
+    public void Method()
+    {
+        int correctVar = 0;
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+    }
+
+    [TestClass]
+    public class BreakDebuggerTests
+    {
+        [TestMethod]
+        public async Task BreakDebuggerCall_ReportsDiagnostic()
+        {
+            var test = @"
+public static class SyntaxNodeExtensions
+{
+    public static void BreakDebugger(this object node) {}
+}
+
+public class Test
+{
+    public void Method()
+    {
+        object node = null;
+        {|#0:node.BreakDebugger()|};
+    }
+}";
+
+            var expected = VerifyCS.Diagnostic("BreakDebuggerUsage")
+                .WithLocation(0)
+                .WithMessage("Avoid using 'BreakDebugger' method in production code");
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+    }
+}
---
+++ Practice_Analyzer/Practice_Analyzer/Practice_AnalyzerAnalyzer.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Practice_Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class Practice_AnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        // Диагностики для BreakDebugger
+        public const string DiagnosticId = "BreakDebuggerUsage";
+        private const string BreakDebuggerTitle = "BreakDebugger method call detected";
+        private const string BreakDebuggerMessage = "Avoid using 'BreakDebugger' method in production code";
+        
+        // Диагностики для проверки именования
+        public const string NamingDiagnosticId = "NamingViolation";
+        private const string NamingTitle = "Incorrect naming convention";
+        private const string PrivatePropertyMessage = "Private property '{0}' should start with uppercase letter";
+        private const string EventMessage = "Event '{0}' should start with uppercase letter";
+        private const string ConstMessage = "Constant '{0}' should start with uppercase letter";
+        private const string ClassMessage = "Class '{0}' should start with uppercase letter";
+        private const string LocalVarMessage = "Local variable '{0}' should start with lowercase letter";
+
+        private static readonly DiagnosticDescriptor BreakDebuggerRule = new DiagnosticDescriptor(
+            DiagnosticId, 
+            BreakDebuggerTitle, 
+            BreakDebuggerMessage, 
+            "Usage", 
+            DiagnosticSeverity.Warning, 
+            isEnabledByDefault: true);
+
+        private static readonly DiagnosticDescriptor NamingRule = new DiagnosticDescriptor(
+            NamingDiagnosticId,
+            NamingTitle,
+            "{0}",
+            "Naming",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => 
+            ImmutableArray.Create(BreakDebuggerRule, NamingRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            
+            context.RegisterSyntaxNodeAction(AnalyzeInvocation, SyntaxKind.InvocationExpression);
+            
+            context.RegisterSymbolAction(AnalyzeNamedType, SymbolKind.NamedType);
+            context.RegisterSymbolAction(AnalyzeProperty, SymbolKind.Property);
+            context.RegisterSymbolAction(AnalyzeEvent, SymbolKind.Event);
+            context.RegisterSymbolAction(AnalyzeField, SymbolKind.Field);
+            context.RegisterSyntaxNodeAction(AnalyzeLocalDeclaration, SyntaxKind.LocalDeclarationStatement);
+        }
+
+        private void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
+        {
+            var invocation = (InvocationExpressionSyntax)context.Node;
+            
+            if (invocation.Expression is MemberAccessExpressionSyntax memberAccess &&
+                memberAccess.Name.Identifier.Text == "BreakDebugger")
+            {
+                var methodSymbol = context.SemanticModel.GetSymbolInfo(memberAccess).Symbol as IMethodSymbol;
+                
+                if (methodSymbol?.ContainingType?.Name == "SyntaxNodeExtensions")
+                {
+                    var diagnostic = Diagnostic.Create(
+                        BreakDebuggerRule, 
+                        invocation.GetLocation());
+                    
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+
+        private void AnalyzeNamedType(SymbolAnalysisContext context)
+        {
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            
+            if (namedTypeSymbol.TypeKind == TypeKind.Class && 
+                char.IsLower(namedTypeSymbol.Name[0]))
+            {
+                var diagnostic = Diagnostic.Create(
+                    NamingRule,
+                    namedTypeSymbol.Locations[0],
+                    string.Format(ClassMessage, namedTypeSymbol.Name));
+                
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeProperty(SymbolAnalysisContext context)
+        {
+            var propertySymbol = (IPropertySymbol)context.Symbol;
+            
+            if (propertySymbol.DeclaredAccessibility == Accessibility.Private &&
+                char.IsLower(propertySymbol.Name[0]))
+            {
+                var diagnostic = Diagnostic.Create(
+                    NamingRule,
+                    propertySymbol.Locations[0],
+                    string.Format(PrivatePropertyMessage, propertySymbol.Name));
+                
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeEvent(SymbolAnalysisContext context)
+        {
+            var eventSymbol = (IEventSymbol)context.Symbol;
+            
+            if (char.IsLower(eventSymbol.Name[0]))
+            {
+                var diagnostic = Diagnostic.Create(
+                    NamingRule,
+                    eventSymbol.Locations[0],
+                    string.Format(EventMessage, eventSymbol.Name));
+                
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeField(SymbolAnalysisContext context)
+        {
+            var fieldSymbol = (IFieldSymbol)context.Symbol;
+            
+            if (fieldSymbol.IsConst && char.IsLower(fieldSymbol.Name[0]))
+            {
+                var diagnostic = Diagnostic.Create(
+                    NamingRule,
+                    fieldSymbol.Locations[0],
+                    string.Format(ConstMessage, fieldSymbol.Name));
+                
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeLocalDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+            
+            foreach (var variable in localDeclaration.Declaration.Variables)
+            {
+                if (variable.Identifier.Text.Length > 0 && 
+                    char.IsUpper(variable.Identifier.Text[0]))
+                {
+                    var diagnostic = Diagnostic.Create(
+                        NamingRule,
+                        variable.Identifier.GetLocation(),
+                        string.Format(LocalVarMessage, variable.Identifier.Text));
+                    
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
